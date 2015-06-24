3e851954e5e37375f684e68ca8630c2f2321aaa1
FujiMakoto <makoto@makoto.io>
2015-06-24 04:15:22 -0400

    version number

--- saml/__init__.py
+++ saml/__init__.py
@@ -3,5 +3,5 @@ from .saml import Saml
 __author__     = "Makoto Fujimoto"
 __copyright__  = 'Copyright 2015, Makoto Fujimoto'
 __license__    = "MIT"
-__version__    = "0.1"
+__version__    = "0.1a1"
 __maintainer__ = "Makoto Fujimoto"
