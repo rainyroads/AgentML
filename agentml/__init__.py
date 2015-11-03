c5c733bc9aaf25138425fc788e2b3f26cf304acb
Makoto <FujiMakoto@users.noreply.github.com>
2015-11-03 00:35:24 -0500

    Version bump

--- agentml/__init__.py
+++ agentml/__init__.py
@@ -18,7 +18,7 @@ from agentml.errors import AgentMLError, VarNotDefinedError, UserNotDefinedError
 __author__     = "Makoto Fujimoto"
 __copyright__  = 'Copyright 2015, Makoto Fujimoto'
 __license__    = "MIT"
-__version__    = "0.3"
+__version__    = "0.3.1"
 __maintainer__ = "Makoto Fujimoto"
 
 
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.3',
+    version='0.3.1',
 
     description='An XML dialect for creating natural language software agents',
     # long_description=long_description,
